934425413a788568321474dcb22ead9d6c97520c
Bakyt <djumabaevb@gmail.com>
2021-04-19 15:44:05 +0600

    media player play button

--- app/src/main/java/com/example/playvideo/MainActivity.java
+++ app/src/main/java/com/example/playvideo/MainActivity.java
@@ -3,23 +3,43 @@ package com.example.playvideo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    private SurfaceView surfaceView;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.video);
+        surfaceView = findViewById(R.id.surface_view);
+        surfaceView.setKeepScreenOn(true);
 
+        SurfaceHolder holder = surfaceView.getHolder();
+        holder.addCallback(this);
+        holder.setFixedSize(400,300);
+
+        Button play = findViewById(R.id.button_play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
 
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
+        mediaPlayer.setDisplay(holder);
     }
 
     @Override
