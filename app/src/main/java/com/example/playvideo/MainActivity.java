5b3f2a77286d0f61c7a2f11c2c189de0fa076782
Bakyt <djumabaevb@gmail.com>
2021-04-19 15:26:39 +0600

    surface holder

--- app/src/main/java/com/example/playvideo/MainActivity.java
+++ app/src/main/java/com/example/playvideo/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.playvideo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
     }
 }
